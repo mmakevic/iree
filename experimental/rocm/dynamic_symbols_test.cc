4b26c2d6b8f0007c24940bfaca761ec1f8b5d565
mmakevic <Milica.Makevic@Syrmia.com>
2023-12-15 09:59:11 +0000

    Added test for RCCL dynamic symbols

--- experimental/rocm/dynamic_symbols_test.cc
+++ experimental/rocm/dynamic_symbols_test.cc
@@ -4,7 +4,9 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include <gtest/gtest.h>
 #include "experimental/rocm/rocm_dynamic_symbols.h"
+#include "experimental/rocm/rccl_dynamic_symbols.h"
 
 #include <iostream>
 
@@ -40,6 +42,40 @@ TEST(DynamicSymbolsTest, CreateFromSystemLoader) {
   }
 
   iree_hal_rocm_dynamic_symbols_deinitialize(&symbols);
+}
+
+#define RCCL_CHECK_ERRORS(expr)     \
+  {                                 \
+    ncclResult_t status = expr;     \
+    ASSERT_EQ(ncclSuccess, status); \
+  }         
+
+TEST(RCCLDynamicSymbolsTest, CreateFromSystemLoader) {
+  iree_hal_rocm_dynamic_symbols_t rocm_symbols;
+  iree_status_t status = iree_hal_rocm_dynamic_symbols_initialize(
+      iree_allocator_system(), &rocm_symbols);
+  if(!iree_status_is_ok(status)) {
+    iree_status_fprint(stderr, status);
+    iree_status_ignore(status);
+    std::cerr << "ROCm symbols cannot be loaded, skipping test.";
+    GTEST_SKIP();
+  }
+
+  iree_hal_rocm_rccl_dynamic_symbols_t rccl_symbols;
+  status = iree_hal_rocm_rccl_dynamic_symbols_initialize(
+      iree_allocator_system(), &rocm_symbols, &rccl_symbols);
+  if (!iree_status_is_ok(status)) {
+    iree_status_fprint(stderr, status);
+    iree_status_ignore(status);
+    std::cerr << "ROCm RCCL symbols cannot be loaded, skipping test.";
+    GTEST_SKIP();
+  }
+
+  int rccl_version;
+  RCCL_CHECK_ERRORS(rccl_symbols.ncclGetVersion(&rccl_version));
+  ASSERT_EQ(NCCL_VERSION_CODE, rccl_version);
+  iree_hal_rocm_rccl_dynamic_symbols_deinitialize(&rccl_symbols);
+  iree_hal_rocm_dynamic_symbols_deinitialize(&rocm_symbols);
 }
 
 }  // namespace
